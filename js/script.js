e3d4b7671d5c72da9ce91b1fe914a23a11b7897b
DELL <shivanikumari28@gmail.com>
2023-08-28 20:26:10 +0530

    middle commit

--- js/script.js
+++ js/script.js
@@ -79,3 +79,12 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+window.onload = fadeOut;
